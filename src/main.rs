70d1b4c6c86208d3ebd969a1cee09e08bf5cd0c7
Josh Crim <jshcrm@gmail.com>
2018-02-24 15:16:14 -0500

    add git merge script

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 extern crate clap;
 
 use std::process::Command;
-use clap::{Arg, App};
+use clap::{App, SubCommand};
 
 
 fn main() {
@@ -9,20 +9,53 @@ fn main() {
         .version("0.1")
         .author("Josh C. <jshcrm@gmail.com>")
         .about("Collection of git shortcuts")
-        .arg(Arg::with_name("command")
-            .index(1))
-            .help("Calls git [command]")
+        .subcommand(SubCommand::with_name("merge"))
         .get_matches();
 
-    let command = matches.value_of("command").unwrap();
+    if let Some(_matches) = matches.subcommand_matches("merge") {
+        let mut current_branch = Command::new("git")
+            .arg("rev-parse")
+            .arg("--abbrev-ref")
+            .arg("HEAD")
+            .output()
+            .expect("fucked up");
 
-    println!(" ");
-    println!("{:?}", matches);
-    println!(" ");
-    println!("{}", command);
+        let mut current_branch: String = String::from_utf8_lossy(&current_branch.stdout).to_string();
+        current_branch.pop();
 
-    Command::new("git")
-        .arg(command)
-        .spawn()
-        .expect("fucked up");
+        Command::new("git")
+            .arg("stash")
+            .spawn()
+            .expect("fucked up");
+
+        Command::new("git")
+            .arg("checkout")
+            .arg("master")
+            .spawn()
+            .expect("fucked up");
+
+        Command::new("git")
+            .arg("pull")
+            .spawn()
+            .expect("fucked up");
+
+        Command::new("git")
+            .arg("checkout")
+            .arg(current_branch)
+            .spawn()
+            .expect("fucked up");
+
+        Command::new("git")
+            .arg("merge")
+            .arg("master")
+            .spawn()
+            .expect("fucked up");
+
+        Command::new("git")
+            .arg("stash")
+            .arg("pop")
+            .spawn()
+            .expect("fucked up");
+    }
+
 }
